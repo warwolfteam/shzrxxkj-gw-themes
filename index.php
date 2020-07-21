234a73cf54c6d2f33912bf82c1dbcb025e581521
heimaoteam <zxx@heimaoba.cn>
2020-07-21 15:03:57 +0800

    再来一波

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
 <?php get_header();?>
     <div class="slider">
       <ul class="bxslider">
-        <li><img src="<?php echo get_template_directory_uri()?>/images/s001.jpg" /></li>
+        <li><img src="<?php echo get_template_directory_uri()?>/images/banner001.jpeg" /></li>
         <li><img src="<?php echo get_template_directory_uri()?>/images/s002.jpg" /></li>
-        <li><img src="<?php echo get_template_directory_uri()?>/images/s003.jpg" /></li>
-        <li><img src="<?php echo get_template_directory_uri()?>/images/s004.jpg" /></li>
       </ul>
     </div><!-- .slider -->
     <div class="content">
@@ -25,7 +23,7 @@
           ?>
           <li class="imgsuo">
             <?php if($my_query->have_posts()):while($my_query->have_posts()):$my_query->the_post();?>
-              <a href="<?php the_permalink();?>""><img src="/images/baidu.png" alt="baidu"></a>
+              <a href="<?php the_permalink();?>""><img src="<?php echo get_template_directory_uri();?>/images/baidu.png" alt="baidu"></a>
               <h3><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
               <?php echo lingfeng_strimwidth(get_the_content(), 34);?>
               <div class="clear"></div>
@@ -48,15 +46,10 @@
         </ul>
       </div><!-- .xinxi02 -->
       <div class="module xinxi03">
-        <h2>
-          <a href="<?php echo get_page_link(107);?>">企业荣誉</a>
-          <span><a href="<?php echo get_page_link(107);?>" title="查看详细"></a></span>
-        </h2>
+        <h2>企业荣誉</h2>
         <div class="rongyu">
           <ul class="chuzhi-bxslider">
-            <li><img src="<?php echo get_template_directory_uri();?>/images/honor01.jpg" /></li>
-            <li><img src="<?php echo get_template_directory_uri();?>/images/honor02.jpg" /></li>
-            <li><img src="<?php echo get_template_directory_uri();?>/images/honor03.jpg" /></li>
+            <li><img src="<?php echo get_template_directory_uri();?>/images/ry001.jpg" /></li>
           </ul>
         </div><!-- .rongyu -->
       </div><!-- .xinxi03 -->
@@ -66,10 +59,7 @@
           $my_term_id = $my_term->term_id;
           $my_term_link = get_term_link($my_term_id,'category');
         ?>
-        <h2>
-          <a href="<?php echo $my_term_link;?>">产品展示</a>
-          <span><a href="<?php echo $my_term_link;?>" title="查看详细"></a></span>
-        </h2>
+        <h2>产品展示</h2>
         <ul>
             <?php
               $my_query = new WP_Query(array(
@@ -97,10 +87,7 @@
           $my_term_id = $my_term->term_id;
           $my_term_link = get_page_link($my_term_id,'category');
         ?>
-        <h2>
-          <a href="<?php echo $my_term_link;?>">公司新闻</a>
-          <span><a href="<?php echo $my_term_link;?>" title="查看详细"></a></span>
-        </h2>
+        <h2>公司新闻</h2>
         <ul>
           <?php
             $my_query = new WP_Query(array(
